b49ba2e1a95a7c8e6762d50e3bc3e3e30bba4c4d
Ryan <sailantht@gmail.com>
2022-08-29 09:10:52 +0430

    Glucose Model

--- glucose/models.py
+++ glucose/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
 # Create your models here.
+
+User = get_user_model()
+
+
+class Glucose(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    gerät = models.CharField(max_length=200)
+    seriennummer = models.CharField(max_length=200)
+    aufzeichnungstyp = models.PositiveSmallIntegerField(default=0)
+    glukosewert = models.PositiveSmallIntegerField(help_text='Verlauf mg/dL')
+
+    def __str__(self):
+        return self.user
+
+    class Meta:
+        verbose_name = 'Glucose'
+        verbose_name_plural = 'Glucose'
